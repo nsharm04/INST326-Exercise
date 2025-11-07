66ee32ecc30c06237360c0af39fd335ca6594c71
Navya Sharma <186406016+nsharm04@users.noreply.github.com>
2025-11-07 14:23:09 -0500

    This first commit includes a secret_greeting function which will return a greeting if the correct input is provided

---
+++ hello_all.py
@@ -0,0 +1,8 @@
+def secret_greeting(num):
+    num = int(input("Enter an even number for the secret greeting: "))
+    if num % 2 == 0:
+        print("Hello all! Welcome to this test repository")
+    else: 
+        print("Not a correct or accepted number!")
+    
+secret_greeting(2)
